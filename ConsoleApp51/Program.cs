4a82c3119ab841542afc095bfda8c828c24cedd3
Vikkans <Jakobsenviktor6@gmail.com>
2023-11-28 18:34:07 +0100

    Add project files.

---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+            for (int i = 0; i < tal.Length; i++)
+            {
+                Console.WriteLine("Välj ett tal");
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = tal[0] + tal[1] + tal[2];
+
+            Console.WriteLine("summan av dina tal är " + sum);
+
+        }
+    }
+}
